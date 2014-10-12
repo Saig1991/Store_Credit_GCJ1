3c67d01ecf7739496aa62c5b4e75ebbcc9f5bb5c
Sai <sai96@hotmail.com>
2014-10-12 14:41:56 +1100

    Store.h: Minor clean up of header file

--- Store.h
+++ Store.h
@@ -1,19 +1,21 @@
 #ifndef STORE_H
 #define STORE_H
 
-#include "queue"
+#include <vector>
+#include <queue>
+#include "Customer.h"
 
-public class Store {
+class Store {
 public:    
     //Construct the store its number of items.
-    Store(int numItems)
+    Store(int numItems);
     //Searches the store with the constraint from the customer to find the two objects.
-    std::queue<int> findObjects(Customer &cust)
+    std::queue<int> findObjects(Customer &cust);
     // Add item to store
     void addItem(int item);
 private:
-    std::vector<int> store
-    int numStoreitems;
+    std::vector<int> store;
+    int numStoreItems;
 };
 
 #endif
